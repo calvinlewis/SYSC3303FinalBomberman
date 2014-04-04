cc614421eb2d2c1334ba4766d11d644aed914414
Ali Avci <aliavci96@hotmail.com>
2014-04-04 04:45:07 -0400

    Update PlayerClient.java

--- SYSC3303Bomberman/src/PlayerClient.java
+++ SYSC3303Bomberman/src/PlayerClient.java
@@ -1,33 +1,91 @@
 
-import java.io.BufferedInputStream;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import static java.lang.Thread.sleep;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
 
 
 public class PlayerClient implements Runnable {
 
 	private static Socket clientSocket = null;
-	private static PrintStream os = null;
-	private static DataInputStream is = null;
-
-	private static BufferedReader input = null;
+	public static PrintStream os = null;
+	public static DataInputStream is = null;
+        public static JLabel label;
+        public static JButton bomb = new JButton("bomb");
+        public static JButton left = new JButton("left");
+        public static JButton right = new JButton("right");
+        public static JButton up = new JButton("up");
+        public static JButton down = new JButton("down");
+	public static BufferedReader input = null;
 	private static boolean closed = false;
+	public static JFrame frame = new JFrame("Client console");
+        public static clientbuttons buttonlistener = new clientbuttons ();
 	
-	static File file = new File("/Users/Calvin/Documents/SYSC3303FinalBomberman/SYSC3303Bomberman/scalability.txt");
-	
-
 	public static void main(String[] args) {
-
+                label = new JLabel();
+                
+                label.setFont(new Font("Courier", Font.PLAIN, 12));
+                frame.setMinimumSize(new Dimension(500, 500));
+                frame.setBounds(100,100,450,300);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+                bomb.addActionListener(buttonlistener);
+                up.addActionListener(buttonlistener);
+                down.addActionListener(buttonlistener);
+                left.addActionListener(buttonlistener);
+                right.addActionListener(buttonlistener);
+                
+                GridBagLayout gridBag = new GridBagLayout();
+                GridBagConstraints cons = new GridBagConstraints();
+        
+                cons.fill = GridBagConstraints.BOTH;
+                
+                
+                frame.setLayout(gridBag);
+                
+                cons.gridx = 2;
+                cons.gridy = 1;
+                gridBag.setConstraints(label, cons);
+                frame.add(label);
+                
+                cons.gridx = 2;
+                cons.gridy = 6;
+                gridBag.setConstraints(down, cons);
+                frame.add(down);
+                
+                cons.gridx = 2;
+                cons.gridy = 4;
+                gridBag.setConstraints(up, cons);
+                frame.add(up);
+                
+                cons.gridx = 3;
+                cons.gridy = 5;
+                gridBag.setConstraints(right, cons);
+                frame.add(right);
+                
+                cons.gridx = 2;
+                cons.gridy = 5;
+                gridBag.setConstraints(bomb, cons);
+                frame.add(bomb);
+                
+                cons.gridx = 1;
+                cons.gridy = 5;
+                gridBag.setConstraints(left, cons);
+                frame.add(left);
+                
 		int portNumber = 9000;
 		String host = "localhost";
 		
@@ -37,7 +95,7 @@ public class PlayerClient implements Runnable {
 			host = args[0];
 			portNumber = Integer.valueOf(args[1]).intValue();
 		}
-		makeTestFile();
+		
 		createSocket(host, portNumber);
 	}
 	
@@ -52,10 +110,11 @@ public class PlayerClient implements Runnable {
 			is = new DataInputStream(clientSocket.getInputStream());
 			
 		} catch (UnknownHostException e) {
-			System.err.println("Don't know about host " + host);
+			//System.err.println("Don't know about host " + host);
 			return false;
 		} catch (IOException e) {
-			System.err.println("Couldn't get I/O for the connection to the host" + host);
+			//System.err.println("Couldn't get I/O for the connection to the host "
+			//		+ host);
 			return false;
 		}
 		
@@ -65,10 +124,9 @@ public class PlayerClient implements Runnable {
 
 				new Thread(new PlayerClient()).start();
 				while (!closed) {
-					
 					os.println(input.readLine().trim());
-
 				}
+				
 				os.close();
 				is.close();
 				clientSocket.close();
@@ -80,19 +138,53 @@ public class PlayerClient implements Runnable {
 		}
 		return true;	
 	}
+
+    static void input(InputStreamReader inputStreamReader) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 	
 	@Override
 	public void run() {
 		
 		String responseLine;
+                String[] boardarray = {"TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY","TEMPORARY"};
+                String temp;
+                int i = 0;
+                int j = 0;
 		try {
 			
 			while ((responseLine = is.readLine()) != null) {
-				System.out.println(responseLine);
-
-					
-				if (responseLine.indexOf("END_GAME") != -1)
-					break;
+                            
+                            if(i==14){
+                                while(i>0){
+                                    if(j==0){
+                                        label.setText("");
+                                    }
+                                    temp = label.getText().replaceAll("</html>", "");
+                                    temp = temp.replaceAll("</p>", "");
+                                    temp = temp.replaceAll("<pre>", "");
+                                    temp = temp.replaceAll("</pre>", "");
+                                    temp = temp.replaceAll("<html>", "");
+                                    temp = temp.replaceAll("<p>", "");
+                                    label.setText("<html>" + "<pre>" + "<p>"+ temp + boardarray[j] + " <br>" +"</p>"+ "</pre>" + "</html>");
+                                    i--;
+                                    j++;
+                                }
+                                j = 0;
+                                
+                            }
+                            if(responseLine.length()==15){
+                                boardarray[i]= responseLine;
+                                
+                                i++;
+                            }
+                            else{
+                                i=0;
+                                //label.setText("<html>" + "<p>" + responseLine + "</p>" + "</html>");
+                            }
+                            System.out.println(responseLine);
+                            if (responseLine.indexOf("END_GAME") != -1)
+                                break;
 			}
 			closed = true;
 		} catch (IOException e) {
@@ -103,22 +195,6 @@ public class PlayerClient implements Runnable {
 	public boolean testEmptyBuffer() {
 
 		return createSocket("localhost", 5555);
-	}
-	
-	public static void makeTestFile() {
-		try {
-			BufferedWriter output = new BufferedWriter(new FileWriter(file));
-			output.write("Calvin");
-			output.write("START_GAME");
-			for (int i=0; i<5000; i++) {
-				output.write("UP");
-			}
-			output.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 	}
 
 }
